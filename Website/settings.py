d65ee0e65c124a6b52045e65b0cd4d9fa69ddfcb
João Victor Galindo <joao.galindo@transferoswiss.ch>
2021-11-19 17:12:11 -0300

    Add whitenoise middleware

--- Website/settings.py
+++ Website/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,9 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
+
+MEDIA_ROOT = BASE_DIR / "static/images"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
